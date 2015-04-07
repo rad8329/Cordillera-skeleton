0e1569eef41fc7e0d1c1003214c77e3d3d7eca2e
Robert Díaz <rad8329@gmail.com>
2015-04-07 12:45:15 -0500

    Fix composer package name

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 define('CORDILLERA_APP_DIR', realpath(str_replace('\\', '/', dirname(__FILE__) . '/../..') . DS . 'app') . DS);
-define('CORDILLERA_DIR', realpath(str_replace('\\', '/', dirname(__FILE__) . '/../../vendor/') . DS . 'cordillera') . DS);
+define('CORDILLERA_DIR', realpath(str_replace('\\', '/', dirname(__FILE__) . '/../../vendor/rad8329/') . DS . 'cordillera') . DS);
 define('CORDILLERA_DEBUG', true);
+
+require(__DIR__ . '/../../vendor/autoload.php');
 
 require CORDILLERA_DIR . 'bootstrap.php';
