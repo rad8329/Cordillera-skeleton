4c324babbab2cfbd5ecbc27a1b3ad3c774eb2a1a
Robert Díaz <rad8329@gmail.com>
2018-10-18 14:49:32 -0500

    Heroku ClearDB

--- app/config/web/dev-local.php
+++ app/config/web/dev-local.php
@@ -6,7 +6,7 @@ $config = [
         // IP address instead hostname - Performance UP 100%
         'dsn' => 'mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_656ffe765e1c295;charset=utf8',
         'username' => 'b5c20bd8030ce0',
-        'password' => 'a5ba8363',
+        'password' => '29ea7017c5dc906',
     ],
 ];
 
